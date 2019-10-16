c5d44e1f62884ea0adb447006b5ba9c896bf3bcb
mark36949 <33168791+mark36949@users.noreply.github.com>
2019-10-16 19:58:37 +0800

    day 1
    Mongoose and MongoDB

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+	'mongodb+srv://mark36949:'+
+	 process.env.MONGO_ATLAS_PW +
+	'@cluster0-1u1ip.mongodb.net/test?retryWrites=true&w=majority',
+	{
+		useMongoClient: true
+	}
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
